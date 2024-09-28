cbe907d79faa55a49746174e0668685b1f56a2c4
hunterkasa <79697548+hunterkasa@users.noreply.github.com>
2024-09-29 05:36:49 +0600

    Delete weCode directory

--- weCode/View/contestDetails/cDetails.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var problem = document.getElementById('problem');
-    problem.addEventListener('click', function() {
-        event.preventDefault();
-        //console.log('clicked');
-        //window.location.href = '../problemPage/problemPage.php';
-    });
-});
